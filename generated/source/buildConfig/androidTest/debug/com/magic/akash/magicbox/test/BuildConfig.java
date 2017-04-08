63be7e8c98a436aaaaf6544f8559fb7e2e042515
Akash Singh Sengar <akashsengar786@gmail.com>
2017-04-08 23:10:39 +0530

    Add files via upload

---
+++ generated/source/buildConfig/androidTest/debug/com/magic/akash/magicbox/test/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.magic.akash.magicbox.test;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.magic.akash.magicbox.test";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 7;
+  public static final String VERSION_NAME = "5.01";
+}
